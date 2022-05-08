5dcaed07a658595521b0b64ad86a88dbb8257db8
MarwanabdalR <97983231+MarwanabdalR@users.noreply.github.com>
2022-05-08 23:41:14 +0200

    Marwan_FirstTry
    رفعنا في الميديا اول وتاني ميديا بس ظهروا مش كاملين

---
+++ java/homejava.js
@@ -0,0 +1,35 @@
+function mOver(obj) {
+  obj.innerHTML="Click Me";
+}
+function mOut(obj) {
+  obj.innerHTML="Login Now  <i class='fa-solid fa-right-to-bracket'></i>";
+}
+function box(){
+  alert("wellcome in films area.");
+}
+box();
+
+var img_content = document.getElementById('box');
+var image = ['../media/postergot.jpg',
+  '../media/dont2.jpg',
+  '../media/spong2020/13.jpg',
+  '../media/conji.jpg'];
+var i=0,j=0;
+function nextimg() {
+  document.slideshow.src=image[i];
+  if (i<image.length-1) {
+    i++;
+  }else {
+i=0;
+  }
+
+}
+function prewimge(){
+  document.slideshow.src=image[j];
+ if (j<image.length+1 && ( j>1 || j === 1 ) ){
+       j=j-1;
+   }else{
+j= image.length;
+   }
+}
+setInterval( nextimg , 5000);
